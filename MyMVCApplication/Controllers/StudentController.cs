f4335a89a888ab5843603942e4dd4f776c57cf18
Pratik <pratikbajracharya1999@gamil.com>
2020-05-27 15:00:35 +0800

    initial commit

---
+++ MyMVCApplication/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using MyMVCApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        IList<Student> studentList = new List<Student>() {
+                    new Student(){ StudentId=1, StudentName="John", Age = 18 },
+                    new Student(){ StudentId=2, StudentName="Steve", Age = 21 },
+                    new Student(){ StudentId=3, StudentName="Bill", Age = 25 },
+                    new Student(){ StudentId=4, StudentName="Ram", Age = 20 },
+                    new Student(){ StudentId=5, StudentName="Ron", Age = 31 },
+                    new Student(){ StudentId=6, StudentName="Chris", Age = 17 },
+                    new Student(){ StudentId=7, StudentName="Rob", Age = 19 }
+                };
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(studentList);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            //Get the student from studentList sample collection for demo purpose.
+            //Get the student from the database in the real application
+            var std = studentList.Where(s => s.StudentId == Id).FirstOrDefault();
+
+            return View(std);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            var name = std.StudentName;
+            var age = std.Age; 
+            //write code to update student 
+
+            return RedirectToAction("Index");
+        }
+    }
+}
